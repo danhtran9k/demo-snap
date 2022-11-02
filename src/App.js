000cf87929748b4074929ba901f4b844616fa422
EC2 Default User <ec2-user@ip-172-31-45-52.ap-southeast-1.compute.internal>
2022-11-02 02:44:00 +0000

    feat: add synap editor files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { synapEditorConfig } from "./synapeditor.config"
+import { customConfig } from './customConfig';
 
 function App() {
+  const [editor, setEditor] = useState()
+
+  useEffect(() => {
+    // const script = document.createElement("script")
+    const script = document.getElementById("synapScript")
+    // script.src = "/SynapEditor/synapeditor.min.js"
+    // script.async = true
+    // document.body.appendChild(script)
+    // script.onload = () => {
+    //   const se2 = new window.SynapEditor("synapEditor", synapEditorConfig)
+    // }
+    console.log(window)
+    if (!script) {
+      return
+    }
+    console.count("🚀 App-L23 render")
+    window.editor = new SynapEditor("synapApp", customConfig)
+    // script.addEventListener('load', function () {
+    //   console.count("🚀 App-L23 render")
+    //   const se2 = new window.SynapEditor("synapEditor", synapEditorConfig)
+    //   console.log("🚀 App-L24-se2", se2)
+    // });
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <div id='synapApp' style={{ backgroundColor: '#ffffff', width: '100%', height: '500px' }}> SynapEditor</div>
     </div>
   );
 }
